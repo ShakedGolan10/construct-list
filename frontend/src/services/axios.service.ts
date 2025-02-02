6d7b6d7b62675dae97e6954adfef4b783ff92667
Shaked Golan <shaked518@gmail.com>
2025-02-02 18:47:20 +0200

    added axios.service for api calls

---
+++ frontend/src/services/axios.service.ts
@@ -0,0 +1,50 @@
+import Axios, { AxiosRequestConfig, AxiosResponse } from 'axios'
+
+const BASE_URL: string =
+  process.env.NODE_ENV === 'production'
+    ? '/api/'
+    : '//localhost:3030/api/'
+
+const axiosInstance = Axios.create({
+  baseURL: BASE_URL,
+  withCredentials: true,
+})
+
+export const httpService = {
+  get<T>(endpoint: string, params?: Record<string, any>, query?: Record<string, any>): Promise<T> {
+    return ajax<T>(endpoint, 'GET', params, query)
+  },
+  post<T>(endpoint: string, data: any): Promise<T> {
+    return ajax<T>(endpoint, 'POST', data)
+  },
+  put<T>(endpoint: string, data: any): Promise<T> {
+    return ajax<T>(endpoint, 'PUT', data)
+  },
+  delete<T>(endpoint: string, data?: any): Promise<T> {
+    return ajax<T>(endpoint, 'DELETE', data)
+  },
+}
+
+async function ajax<T>(
+  endpoint: string,
+  method: 'GET' | 'POST' | 'PUT' | 'DELETE' = 'GET',
+  data: any = null,
+  query?: Record<string, any>
+): Promise<T> {
+  try {
+    const config: AxiosRequestConfig = {
+      url: `${BASE_URL}${endpoint}${((method === 'GET' || method === 'DELETE') && data) ? `/${data}` : ''}`,
+      method,
+      data: (method === 'POST' || method === 'PUT') ? data : undefined,
+      params: method === 'GET' ? query : undefined,
+    }
+    const res: AxiosResponse<T> = await axiosInstance(config)
+    return res.data
+  } catch (err: any) {
+    if (err.response?.status === 401) {
+      sessionStorage.clear()
+    }
+    console.error('HTTP Error:', err)
+    throw err
+  }
+}
