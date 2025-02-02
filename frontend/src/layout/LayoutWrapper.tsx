be0c1a0cf1d702c4c811c1f7d807e3376b36ca67
Shaked Golan <shaked518@gmail.com>
2025-02-02 02:53:38 +0200

    mistake - should commited earlier

--- frontend/src/layout/LayoutWrapper.tsx
+++ frontend/src/layout/LayoutWrapper.tsx
@@ -15,12 +15,12 @@ export default function MainLayout({ children }: MainLayoutProps) {
         grid 
         gap-4 
         grid-cols-[repeat(auto-fill,_minmax(250px,_1fr))]
-        px-[150px]   /* 150px padding left/right */
-        pt-[150px]   /* 150px padding top */
-        tablet:px-12 /* reduce to ~48px left/right on tablet */
-        tablet:pt-12 /* ~48px top on tablet */
-        mobile:px-4  /* ~16px left/right on mobile */
-        mobile:pt-0  /* remove top padding on mobile */
+        px-[150px]   
+        pt-[150px]   
+        tablet:px-12 
+        tablet:pt-12 
+        mobile:px-4  
+        mobile:pt-0  
       "
     >
       {children}
